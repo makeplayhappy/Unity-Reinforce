617b6bac26a64c4babf0025dc390fe6e7dc997b4
makeplayhappy <smile@makeplayhappy.com>
2021-12-24 09:25:57 +0000

    Initial Matrix functions

--- Assets/Reinforce/Recurrent/Mat.cs
+++ Assets/Reinforce/Recurrent/Mat.cs
@@ -1,49 +1,52 @@
+using System.Diagnostics;
 namespace Recurrent{
 
     public class Mat {
     
-    public readonly rows: number;
-    public readonly cols: number;
-    private readonly _length: number;  // length of 1d-representation of Mat
+        public readonly int rows;
+        public readonly int cols;
+        private readonly int _length;  // length of 1d-representation of Mat
 
-    public readonly w: Array<number> | Float64Array;
-    public readonly dw: Array<number> | Float64Array;
+        public readonly float[] w;
+        public readonly float[] dw;
 
-    /**
-    * 
-    * @param rows rows of Matrix
-    * @param cols columns of Matrix
-    */
-    constructor(rows: number, cols: number) {
-        super();
-        this.rows = rows;
-        this.cols = cols;
-        this._length = rows * cols;
-        this.w = Utils.zeros(this._length);
-        this.dw = Utils.zeros(this._length);
-    }
+        /**
+        * 
+        * @param rows rows of Matrix
+        * @param cols columns of Matrix
+        */
+        Mat(int rows, int cols) {
+            //super();
+            this.rows = rows;
+            this.cols = cols;
+            this._length = rows * cols;
+            this.w = Utils.zeros(this._length);
+            this.dw = Utils.zeros(this._length);
+        }
 
-    /**
-    * Accesses the value of given row and column.
-    * @param row 
-    * @param col
-    * @returns the value of given row and column
-    */
-    public get(row: number, col: number): number {
-        const ix = this.getIndexBy(row, col);
-        Mat.assert(ix >= 0 && ix < this.w.length, '[class:Mat] get: index out of bounds.');
-        return this.w[ix];
-    }
+        /**
+        * Accesses the value of given row and column.
+        * @param row 
+        * @param col
+        * @returns the value of given row and column
+        */
+        public int get(int row, int col) {
+            int ix = this.getIndexBy(row, col);
+            //Mat.assert(ix >= 0 && ix < this.w.Length, '[class:Mat] get: index out of bounds.');
+            Debug.Assert(ix >= 0 && ix < this.w.Length, "[class:Mat] get: index out of bounds.");
+            return this.w[ix];
+        }
 
-    /**
-    * Mutates the value of given row and column.
-    * @param row 
-    * @param col 
-    * @param v 
-    */
-        public set(row: number, col: number, v: number): void {
-            const ix = this.getIndexBy(row, col);
-            Mat.assert(ix >= 0 && ix < this.w.length, '[class:Mat] set: index out of bounds.');
+        /**
+        * Mutates the value of given row and column.
+        * @param row 
+        * @param col 
+        * @param v 
+        */
+        public void set(int row, int col, float v) {
+            int ix = this.getIndexBy(row, col);
+            //Mat.assert(ix >= 0 && ix < this.w.Length, '[class:Mat] set: index out of bounds.');
+            Debug.Assert(ix >= 0 && ix < this.w.Length, "[class:Mat] set: index out of bounds.");
             this.w[ix] = v;
         }
 
@@ -52,7 +55,7 @@ namespace Recurrent{
         * @param row 
         * @param col 
         */
-        protected getIndexBy(row: number, col: number): number {
+        protected int getIndexBy(int row, int col) {
             return (row * this.cols) + col;
         }
 
@@ -60,9 +63,9 @@ namespace Recurrent{
         * Sets values according to the given Array.
         * @param arr 
         */
-        public setFrom(arr: Array<number> | Float64Array): void {
-            for (let i = 0; i < arr.length; i++) {
-            this.w[i] = arr[i];
+        public void setFrom(float[] arr) {
+            for (int i = 0; i < arr.Length; i++) {
+                this.w[i] = arr[i];
             }
         }
 
@@ -71,10 +74,11 @@ namespace Recurrent{
         * @param m 
         * @param colIndex 
         */
-        public setColumn(m: Mat, colIndex: number): void {
-            Mat.assert(m.w.length === this.rows, '[class:Mat] setColumn: dimensions misaligned.');
-            for (let i = 0; i < m.w.length; i++) {
-            this.w[(this.cols * i) + colIndex] = m.w[i];
+        public void setColumn(Mat m, int colIndex) {
+            Debug.Assert(m.w.Length == this.rows, "[class:Mat] setColumn: dimensions misaligned.");
+            //Mat.assert(m.w.Length === this.rows, '[class:Mat] setColumn: dimensions misaligned.');
+            for (int i = 0; i < m.w.Length; i++) {
+                this.w[(this.cols * i) + colIndex] = m.w[i];
             }
         }
 
@@ -85,33 +89,35 @@ namespace Recurrent{
         * @param {Mat} m Matrix to be compared with
         * @returns {boolean} true if equal and false otherwise
         */
-        public equals(m: Mat): boolean {
-            if(this.rows !== m.rows || this.cols !== m.cols) {
-            return false;
-            }
-            for(let i = 0; i < this._length; i++) {
-            if(this.w[i] !== m.w[i]) {
+        public bool equals(Mat m) {
+            if(this.rows != m.rows || this.cols != m.cols) {
                 return false;
             }
+            for(int i = 0; i < this._length; i++) {
+                if(this.w[i] != m.w[i]) {
+                    return false;
+                }
             }
             return true;
         }
 
-        public static toJSON(m: Mat | any): {rows, cols, w} {
-            const json = {rows: 0, cols: 0, w: []};
-            json.rows = m.rows || m.n;
-            json.cols = m.cols || m.d;
-            json.w = m.w;
+        // mph - JSONS TODO
+        public static string toJSON(Mat m) { // : {rows, cols, w}
+            string json = "";//{rows: 0, cols: 0, w: []};
+            //json.rows = m.rows || m.n;
+            //json.cols = m.cols || m.d;
+            //json.w = m.w;
             return json;
         }
 
-        public static fromJSON(json: {rows, n?, cols, d?, w}): Mat {
-            const rows = json.rows || json.n;
-            const cols = json.cols || json.d;
-            const mat = new Mat(rows, cols);
-            for (let i = 0; i < mat._length; i++) {
-            mat.w[i] = json.w[i];
-            }
+        public static Mat fromJSON(string json) { // : {rows, n?, cols, d?, w}
+            //const rows = json.rows || json.n;
+            //const cols = json.cols || json.d;
+            int row,cols = 1;
+            Mat mat = new Mat(rows, cols);
+            //for (let i = 0; i < mat._length; i++) {
+            //    mat.w[i] = json.w[i];
+            //}
             return mat;
         }
 
@@ -119,12 +125,12 @@ namespace Recurrent{
         * Discounts all values as follows: w[i] = w[i] - (alpha * dw[i])
         * @param alpha discount factor
         */
-        public update(alpha: number): void {
-            for (let i = 0; i < this._length; i++) {
-            if (this.dw[i] !== 0) {
-                this.w[i] = this.w[i] - alpha * this.dw[i];
-                this.dw[i] = 0;
-            }
+        public void update(float alpha) {
+            for (int i = 0; i < this._length; i++) {
+                if (this.dw[i] != 0) {
+                    this.w[i] = this.w[i] - alpha * this.dw[i];
+                    this.dw[i] = 0f;
+                }
             }
         }
 
